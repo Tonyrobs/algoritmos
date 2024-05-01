bcbbaf0b02a51dcd8420bd6359c37013f9801c7d
Anthony <58789173+Tonyrobs@users.noreply.github.com>
2024-05-01 15:12:40 -0300

    Update

--- ALP/Tabuada de um número.cpp
+++ ALP/Tabuada de um número.cpp
@@ -3,21 +3,14 @@ using namespace std;
 
 int main()
 {
-  int num=0;
-  
-    cout<<"Tabuada de um numero positivo\n";
-    cout<<"Insira um numero: ";
-    cin>>num;
-    cout<<"\nA tabuada do numero sera:\n";
-    cout<<num<<" x 1 = "<<num*1<<"\n";
-    cout<<num<<" x 2 = "<<num*2<<"\n";
-    cout<<num<<" x 3 = "<<num*3<<"\n";
-    cout<<num<<" x 4 = "<<num*4<<"\n";
-    cout<<num<<" x 5 = "<<num*5<<"\n";
-    cout<<num<<" x 6 = "<<num*6<<"\n";
-    cout<<num<<" x 7 = "<<num*7<<"\n";
-    cout<<num<<" x 8 = "<<num*8<<"\n";
-    cout<<num<<" x 9 = "<<num*9<<"\n";
-    cout<<num<<" x 10 = "<<num*10;    
-    
+	int n=0, mult=0;
+	cout<<"Tabuada de um numero\n";
+	cout<<"Insira numero: ";
+	cin>>n;
+	
+	for(int cont=1; cont<=10; cont++)
+	{
+		mult=cont*n;
+		cout<<"\n"<<n<<" x "<<cont<<" = "<<mult;
+	}
 }
