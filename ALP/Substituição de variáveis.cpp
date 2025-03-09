ab742eb8c02eddc47b3eb1620ea4cb277a79be83
Anthony Martins <58789173+Tonyrobs@users.noreply.github.com>
2025-03-08 21:48:28 -0300

    Update Substituição de variáveis.cpp

--- ALP/Substituição de variáveis.cpp
+++ ALP/Substituição de variáveis.cpp
@@ -3,12 +3,15 @@ using namespace std;
 
 int main()
 {
-    int a=0, b=0;
+    int a=0, b=0, temp=0;
     
         cout<<"Substituição de valores\n";
         cout<<"Insira o número A: ";
         cin>>a;
         cout<<"Insira o número B: ";
         cin>>b;
+        temp=a;
+        a=b;
+        b=temp;
         cout<<"Após a substituição, o valor A é: "<< b<< " e o valor B é: "<<a;
 }
