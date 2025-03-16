0d8e7e8d5f648c73f695be3499619b587a5a30b3
Anthony Martins <58789173+Tonyrobs@users.noreply.github.com>
2025-03-16 16:26:46 -0300

    Update exercicio3.c

--- Estrutura de Dados/Pilhas/exercicio3.c
+++ Estrutura de Dados/Pilhas/exercicio3.c
@@ -4,29 +4,29 @@
 #include "pilha.h"
 
 int main() {
-    char frase[100]; // Buffer para armazenar a frase
+    char frase[100];
     int i;
 
     printf("Digite a frase: ");
-    fgets(frase, sizeof(frase), stdin); // Lê a frase inteira, incluindo espaços
+    fgets(frase, sizeof(frase), stdin); 
 
-    int len = strlen(frase); // Obtém o comprimento da frase
-    Pilha A = pilha(len); // Cria uma pilha com capacidade suficiente para a frase
+    int len = strlen(frase); 
+    Pilha A = pilha(len); 
 
     for (i = 0; i < len; i++) {
         if (frase[i] != ' ' && frase[i] != '\n') {
             // Empilha cada caractere da palavra
             empilha(frase[i], A);
         } else {
-            // Quando encontrar um espaço ou nova linha, desempilha e imprime os caracteres
+            
             while (!vaziap(A)) {
                 printf("%c", desempilha(A));
             }
-            // Imprime o espaço ou nova linha
+            
             printf("%c", frase[i]);
         }
     }
-    // Desempilha e imprime qualquer caractere restante na pilha
+    
     while (!vaziap(A)) {
         printf("%c", desempilha(A));
     }
